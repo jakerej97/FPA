fdc4d0f571840aaf3aee47b52e523e44a45c832b
Jake Rejwan <jrejwan3@gatech.edu>
2020-01-20 14:23:13 -0500

    Final prototype working

--- FPA/ViewController.swift
+++ FPA/ViewController.swift
@@ -12,26 +12,62 @@ import FirebaseDatabase
 class ViewController: UIViewController {
     
     var ref: DatabaseReference!
-
+    
+    @IBOutlet weak var TextField: UITextField!
+    //@IBOutlet weak var TextField2: UITextField!
+    
+    let lowerBound = 1
+    let upperBound = 100
+    var numberToGuess: Int!
+    var key: String?
+    var numberOfGuesses = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         ref = Database.database().reference().child("users")
+        generateRandomNumber()
+        getKey()
         
     }
     
+    func generateRandomNumber() {
+        numberToGuess = Int(arc4random_uniform(100)) + 1
+    }
+    
+    func getKey() {
+        key = ref.childByAutoId().key
+    }
+    
     func addUser() {
-        let key = ref.childByAutoId().key
-        
-        let user = ["id": key,
-                    "name": "Jake"]
+        let user = ["id": key!,
+                    "correctNumber": TextField.text!,
+                    "guesses": String(numberOfGuesses)]
         
         ref.child(key!).setValue(user)
         
     }
-
+    
     @IBAction func Button1(_ sender: Any) {
-        addUser()
+        if TextField.text != "" {
+            numberOfGuesses += 1
+            if Int(TextField.text!) == numberToGuess {
+                print("You Win!")
+                addUser()
+            } else {
+                let guess = Int(TextField.text!)
+                if guess! < lowerBound || guess! > upperBound {
+                    print("Your guess should be between 1 and 100!")
+                } else if guess! < numberToGuess {
+                    print("Higher!")
+                } else if guess! > numberToGuess {
+                    print("Lower!")
+                }
+            }
+        } else {
+            print("Please Fill Out All Fields")
+        }
+
     }
     
 }
