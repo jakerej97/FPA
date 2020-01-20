7b2ddecd35ebd48dd3955f251d1e826c588a800c
Jake Rejwan <jrejwan3@gatech.edu>
2020-01-19 20:12:53 -0500

    It's working!

--- FPA/ViewController.swift
+++ FPA/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+    
+    var ref: DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ref = Database.database().reference().child("users")
+        
+    }
+    
+    func addUser() {
+        let key = ref.childByAutoId().key
+        
+        let user = ["id": key,
+                    "name": "Jake"]
+        
+        ref.child(key!).setValue(user)
+        
     }
 
-
+    @IBAction func Button1(_ sender: Any) {
+        addUser()
+    }
+    
 }
 
